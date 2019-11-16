1155ccbb836043b8445747a597d76aa1a7e715c2
Craig Donavin <crdonavin@gmail.com>
2019-11-16 05:23:30 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -2,7 +2,55 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
+let glyphStates = {
+  '♡':'♥',
+  '♥':'♡'
+}
+
+let colorStates = {
+  "red" : "",
+  "": "red"
+};
+
+let articleHearts = document.querySelectorAll('.like')
+
+function liker(e) {
+  let heart = e.target
+  mimicServerCall().then(() => {
+    if (EMPTY_HEART === heart.innerText) {
+      heart.innerText = FULL_HEART
+      heart.classList.add("activated-heart")
+    } else {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove("activated-heart")
+    }
+} )
+    .catch(function (error) {
+      document.getElementById("modal").className = "";
+      setTimeout(function () {
+        document.getElementById("modal").className = "hidden";
+      }, 5000);
+    })
+}
+
+for (let e of articleHearts) { e.addEventListener("click", liker) }
+
+//------------------------------------------------------------------------------
+// Ignore after this point. Used only for demo purposes
+//------------------------------------------------------------------------------
+
+function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+  return new Promise(function(resolve, reject) {
+    setTimeout(function() {
+      let isRandomFailure = Math.random() < .2
+      if (isRandomFailure) {
+        reject("Random server error. Try again.");
+      } else {
+        resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
 
 
 
